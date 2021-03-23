cf2b043a7bd90afe6c3dab4de09a64724162f89b
PAUL-OMIKUNLE <72666596+PAUL-OMIKUNLE@users.noreply.github.com>
2021-03-23 02:50:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask,render_template,request,url_for
+import pandas as pd 
+import numpy as np 
 
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('/predict',methods=["GET","POST"])
+
+
+	return render_template('index.html',prediction=my_prediction,pred_score=pred_score,verse_sentiment=verse_sentiment,raw_text=raw_text)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+© 2021 GitHub, Inc.
